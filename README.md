bd6707fe637d8b1b3347b54ea6ac792fed7d26fb
klierx <klierbyck@gmail.com>
2025-08-09 17:35:31 +0800

    init

---
+++ README.md
@@ -0,0 +1,22 @@
+# clash客户端分流规则配置—让你的clash更简单好用！
+## clash-verge-rev[下载](https://github.com/clash-verge-rev/clash-verge-rev/releases)
+
+# 说明
+## 项目目录中有两个配置文件，分别是script_http（线上分流规则集）和script_local(本地分流规则集，需要下载到本地使用)，建议先试用线上分流规则集，熟练后再使用本地分流规则集自定义属于自己的规则集
+
+# config
+## 打开客户端->点击订阅->右击全局扩展脚本->编辑文件->将项目中的script_xxx文件内容复制到编辑框中，然后点击保存即可
+## 若使用script_http则不需要任何操作即可开启你的分流之旅
+![预览](./img/img1.png)
+## 若使用script_local将项目中的ruleset目录及所有文件复制到配置目录
+![预览](./img/img2.png)
+![预览](./img/img3.png)
+
+# 使用
+## 点击代理->规则->找到对应分组，选择对应规则即可（目前有AI、Github、Telegram、流媒体、国外五个规则集）记得国内选DIRECT
+![预览](./img/img4.png)
+## 
+
+# 如有问题联系QQ: 740411080
+
+# 如果没有好用的机场可以尝试一下（[流量光](https://llgjc1.com/#/register?code=ZC9Klt1L)）还不错，20元1年，每月60G流量，链接需番茄上网才能打开
---
+++ script_local.js
@@ -0,0 +1,318 @@
+// Define main function (script entry)
+// function main(config, profileName) {
+//     return config;
+//   }
+
+// DNS配置
+const dnsConfig = {
+  // 开关，true表示启用Clash的DNS处理器
+  enable: true,
+  // 主DNS服务器组，用于解析国内域名，以获取最快的CDN节点。
+  // 使用加密DNS (DoH/DoT) 可以防止ISP的DNS污染。
+  nameserver: [
+    "https://dns.alidns.com/dns-query", // 阿里DNS (DoH)
+    "https://doh.pub/dns-query", // 腾讯DNSPod (DoH)
+    "tls://223.5.5.5:853", // 阿里DNS (DoT)
+  ],
+  // 备用DNS服务器组。当主DNS解析结果不理想（如被污染或IP归属地非中国）时，
+  // Clash会使用此组DNS进行再次查询，以获取真实、无污染的海外IP。
+  fallback: [
+    "https://dns.google/dns-query", // Google DNS (DoH)
+    "https://1.1.1.1/dns-query", // Cloudflare DNS (DoH)
+    "tls://8.8.4.4:853", // Google DNS (DoT)
+  ],
+};
+
+// 代理组通用配置
+const groupBaseOption = {
+  interval: 300,
+  timeout: 1800,
+  url: "http://www.gstatic.com/generate_204",
+  lazy: true, // 分组首次被实际使用（即有网络请求通过它）时或者当用户手动触发延迟测试时，才会进行一次测速,如果分组少可以改成false或者在对应分组中覆盖此配置
+  "max-failed-times": 3,
+  hidden: false,
+  interval: 120, // 测速间隔，单位秒
+  tolerance: 60, //容差,它的主要作用是 防止因网络波动导致代理节点频繁切换，增加稳定性,单位是毫秒
+};
+
+// 自动测速配置
+const groupBaseAutoTest = {
+  ...groupBaseOption,
+  lazy: false,
+};
+
+// 规则集通用配置
+const ruleProviderCommon = {
+  type: "http",
+  format: "yaml",
+  interval: 86400,
+};
+
+// 规则集配置
+const ruleProviders = {
+  ai: {
+    ...ruleProviderCommon,
+    behavior: "classical",
+    type: "file",
+    path: "./ruleset/ai.yaml",
+  },
+  github: {
+    ...ruleProviderCommon,
+    behavior: "classical",
+    type: "file",
+    path: "./ruleset/github.yaml",
+  },
+  telegram: {
+    ...ruleProviderCommon,
+    behavior: "classical",
+    type: "file",
+    path: "./ruleset/telegram.yaml",
+  },
+  media: {
+    ...ruleProviderCommon,
+    behavior: "classical",
+    type: "file",
+    path: "./ruleset/media.yaml",
+  },
+  global: {
+    ...ruleProviderCommon,
+    behavior: "classical",
+    type: "file",
+    path: "./ruleset/global.yaml",
+  },
+};
+
+// 规则
+const rules = [
+  "RULE-SET,ai,👽 AI",
+  "RULE-SET,github,📘 GitHub",
+  "RULE-SET,telegram,🙋 Telegram",
+  "RULE-SET,media,📀 流媒体",
+  "RULE-SET,global,🌍 国外",
+  "MATCH,➡️ 国内",
+];
+
+// 程序入口
+function main(config) {
+  const proxyCount = config?.proxies?.length ?? 0;
+  const proxyProviderCount =
+    typeof config?.["proxy-providers"] === "object"
+      ? Object.keys(config["proxy-providers"]).length
+      : 0;
+  if (proxyCount === 0 && proxyProviderCount === 0) {
+    throw new Error("配置文件中未找到任何代理");
+  }
+
+  // 覆盖原配置中DNS配置
+  config["dns"] = dnsConfig;
+
+  // 覆盖原配置中的代理组
+  config["proxy-groups"] = [
+    {
+      ...groupBaseAutoTest,
+      name: "所有-自动",
+      type: "url-test",
+      proxies: [],
+      "include-all": true,
+    },
+    {
+      ...groupBaseAutoTest,
+      name: "港台日新韩-自动",
+      type: "url-test",
+      proxies: [],
+      "include-all": true,
+      filter:
+        "(广港|广台|广日|广新|广韩|广美|香港|HK|Hong Kong|🇭🇰|HongKong|台湾|TW|Tai Wan|🇹🇼|🇨🇳|TaiWan|Taiwan|日本|JP|川日|东京|大阪|泉日|埼玉|沪日|深日|🇯🇵|Japan|新加坡|SG|坡|狮城|🇸🇬|Singapore|韩国|KR|首尔|春川|🇰🇷|Korea)",
+    },
+    {
+      ...groupBaseAutoTest,
+      name: "台日新韩-自动",
+      type: "url-test",
+      proxies: [],
+      "include-all": true,
+      filter:
+        "(广台|广日|广新|广韩|广美|台湾|TW|Tai Wan|🇹🇼|🇨🇳|TaiWan|Taiwan|日本|JP|川日|东京|大阪|泉日|埼玉|沪日|深日|🇯🇵|Japan|新加坡|SG|坡|狮城|🇸🇬|Singapore|韩国|KR|首尔|春川|🇰🇷|Korea)",
+    },
+    {
+      ...groupBaseAutoTest,
+      name: "日新韩-自动",
+      type: "url-test",
+      proxies: [],
+      "include-all": true,
+      filter:
+        "(广台|广日|广新|广韩|广美|日本|JP|川日|东京|大阪|泉日|埼玉|沪日|深日|🇯🇵|Japan|新加坡|SG|坡|狮城|🇸🇬|Singapore|韩国|KR|首尔|春川|🇰🇷|Korea)",
+    },
+    {
+      ...groupBaseOption,
+      name: "香港-自动",
+      type: "url-test",
+      proxies: [],
+      "include-all": true,
+      filter: "(广港|香港|HK|Hong Kong|🇭🇰|HongKong)",
+    },
+    {
+      ...groupBaseOption,
+      name: "台湾-自动",
+      type: "url-test",
+      proxies: [],
+      "include-all": true,
+      filter: "(广台|台湾|台灣|TW|Tai Wan|🇹🇼|🇨🇳|TaiWan|Taiwan)",
+    },
+    {
+      ...groupBaseOption,
+      name: "日本-自动",
+      type: "url-test",
+      proxies: [],
+      "include-all": true,
+      filter: "(广日|日本|JP|川日|东京|大阪|泉日|埼玉|沪日|深日|🇯🇵|Japan)",
+    },
+    {
+      ...groupBaseOption,
+      name: "新加坡-自动",
+      type: "url-test",
+      proxies: [],
+      "include-all": true,
+      filter: "(广新|新加坡|SG|坡|狮城|🇸🇬|Singapore)",
+    },
+    {
+      ...groupBaseOption,
+      name: "韩国-自动",
+      type: "url-test",
+      proxies: [],
+      "include-all": true,
+      filter: "(广韩|韩国|韓國|KR|首尔|春川|🇰🇷|Korea)",
+    },
+    {
+      ...groupBaseAutoTest,
+      name: "美国-自动",
+      type: "url-test",
+      proxies: [],
+      "include-all": true,
+      filter:
+        "(广美|美|US|纽约|波特兰|达拉斯|俄勒|凤凰城|费利蒙|硅谷|拉斯|洛杉|圣何塞|圣克拉|西雅|芝加|🇺🇸|United States)",
+    },
+    {
+      ...groupBaseOption,
+      name: "其他-自动",
+      type: "url-test",
+      proxies: [],
+      "include-all": true,
+      filter:
+        "(波|柬|尼|也|克|比|尔|立|冰|秘|耳|利|埃|希|孟|芬|愛|澳|英|德|南|意|法|拿|墨|印|越|俄|瑞|智|荷|比|巴|沙|班|泰|德|烏|以|Australia|Konghwaguk)",
+    },
+    {
+      ...groupBaseOption,
+      name: "👽 AI",
+      type: "select",
+      proxies: [
+        "港台日新韩-自动",
+        "台日新韩-自动",
+        "日新韩-自动",
+        "美国-自动",
+        "其他-自动",
+      ],
+    },
+    {
+      ...groupBaseOption,
+      name: "📘 GitHub",
+      type: "select",
+      proxies: [
+        "DIRECT",
+        "所有-自动",
+        "港台日新韩-自动",
+        "台日新韩-自动",
+        "香港-自动",
+        "台湾-自动",
+        "日本-自动",
+        "新加坡-自动",
+        "韩国-自动",
+        "美国-自动",
+        "其他-自动",
+        "REJECT",
+      ],
+    },
+    {
+      ...groupBaseOption,
+      name: "🙋 Telegram",
+      type: "select",
+      proxies: [
+        "DIRECT",
+        "所有-自动",
+        "港台日新韩-自动",
+        "台日新韩-自动",
+        "香港-自动",
+        "台湾-自动",
+        "日本-自动",
+        "新加坡-自动",
+        "韩国-自动",
+        "美国-自动",
+        "其他-自动",
+        "REJECT",
+      ],
+    },
+    {
+      ...groupBaseOption,
+      name: "📀 流媒体",
+      type: "select",
+      proxies: [
+        "DIRECT",
+        "所有-自动",
+        "港台日新韩-自动",
+        "台日新韩-自动",
+        "香港-自动",
+        "台湾-自动",
+        "日本-自动",
+        "新加坡-自动",
+        "韩国-自动",
+        "美国-自动",
+        "其他-自动",
+        "REJECT",
+      ],
+    },
+    {
+      ...groupBaseOption,
+      name: "🌍 国外",
+      type: "select",
+      proxies: [
+        "DIRECT",
+        "所有-自动",
+        "港台日新韩-自动",
+        "台日新韩-自动",
+        "香港-自动",
+        "台湾-自动",
+        "日本-自动",
+        "新加坡-自动",
+        "韩国-自动",
+        "美国-自动",
+        "其他-自动",
+        "REJECT",
+      ],
+    },
+    {
+      ...groupBaseOption,
+      name: "➡️ 国内",
+      type: "select",
+      proxies: [
+        "DIRECT",
+        "所有-自动",
+        "港台日新韩-自动",
+        "台日新韩-自动",
+        "香港-自动",
+        "台湾-自动",
+        "日本-自动",
+        "新加坡-自动",
+        "韩国-自动",
+        "美国-自动",
+        "其他-自动",
+        "REJECT",
+      ],
+    },
+  ];
+
+  // 覆盖原配置中的规则
+  config["rule-providers"] = ruleProviders;
+  config["rules"] = rules;
+
+  // 返回修改后的配置
+  return config;
+}
